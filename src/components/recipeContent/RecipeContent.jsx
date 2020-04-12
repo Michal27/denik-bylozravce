ecec4bb83ba663c033b61db2c2c7f22e4b07680b
Michal Malanik <michal.malanik@firma.seznam.cz>
2020-04-12 16:19:58 +0200

    #! fixing component recipe content height

--- src/components/recipeContent/RecipeContent.jsx
+++ src/components/recipeContent/RecipeContent.jsx
@@ -6,17 +6,21 @@ class RecipeContent extends React.Component {
 	render() {
 		return (
 			<div className = 'component-recipe-content'>
-				<div className = 'component-recipe-content-left'>
-					<SecondaryHeadline text='Postup'/>
+				<div className = 'component-recipe-content-row'>
+					<div className = 'component-recipe-content-left'>
+						<SecondaryHeadline text='Postup'/>
+					</div>
+					<div className = 'component-recipe-content-right'>
+						<SecondaryHeadline text='Ingredience'/>
+					</div>
 				</div>
-				<div className = 'component-recipe-content-right'>
-					<SecondaryHeadline text='Ingredience'/>
-				</div>
-				<div className = 'component-recipe-content-left'>
-					<SecondaryHeadline text='Další recepty'/>
-				</div>
-				<div className = 'component-recipe-content-right'>
-					<SecondaryHeadline text='Kategorie receptu'/>
+				<div className = 'component-recipe-content-row'>
+					<div className = 'component-recipe-content-left'>
+						<SecondaryHeadline text='Další recepty'/>
+					</div>
+					<div className = 'component-recipe-content-right'>
+						<SecondaryHeadline text='Kategorie receptu'/>
+					</div>
 				</div>
 			</div>
 		);
